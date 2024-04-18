6941ec0a41a7d2ee4c77d1fee3d2f249802b05d1
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-04-18 16:01:03 -0500

    feat: :tada: Ejercicio basico JavaScript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//function sumarRango (numeroInicial, numeroFinal) {
+    
+//}
+// const saludar = funtion(numeroInicial, numeroFinal){
+
+//} 
+import prompt from 'async-prompt'
+//import {sumarRango} from"./module/sumarRangoDeNumeros.js"
+
+
+let num1 = Number(await prompt("Ingrese el numero 1 "))
+let num2 = Number(await prompt("Ingrese el numero 2 "))
+
+console.log(num1)
+//console.log(sumarRango(0, 10));
---
+++ js/module/sumarRangoDeNumeros.js
@@ -0,0 +1,8 @@
+export const sumarRango = (numeroInicial, numeroFinal) => {
+    let resultado = 0;
+    for (let i = numeroInicial; i <= numeroFinal; i++) {
+        resultado += 1
+    }
+    return resultado;
+}
+
